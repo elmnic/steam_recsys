1a8dfb7d3aa4ad6f7eadfe3fa1607d1221cb3f14
Niclas Elmäng <nicke_elmang@hotmail.com>
2020-02-25 11:20:54 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Steam recommender system based on unique play time per user',
+    author='HIS',
+    license='MIT',
+)
